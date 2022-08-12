ba4dff17246a55825d802fdbc2f46841bb5124c4
vinceTheProgrammer <vincetheprogrammer@gmail.com>
2022-08-12 04:30:49 -0500

    feat: add recently added figures and random stickfigure command also fixed the ping command

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const enum StickNodesCategories {
+    BACKGROUNDS = 'backgrounds',
+    EFFECTS = 'effects',
+    MISCELLANEOUS = 'miscellaneous',
+    OBJECTS = 'objects',
+    PACKS = 'packs',
+    PEOPLE = 'people',
+    WEAPONS = 'weapons',
+    VEHICLES = 'vehicles'
+}
+
+export const enum Emojis {
+    LOADING_ANIMATED_COURTESY_OF_DRAGONITE = '<a:_:730555789730775042>'
+}
