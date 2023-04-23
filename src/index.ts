967f903a7f88743993feccf254ae0876458a3951
vinceTheProgrammer <vincetheprogrammer@gmail.com>
2023-04-23 15:19:11 -0500

    BREAKING CHANGE: remove sn site related functionality

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 require('dotenv').config();
 import { LogLevel, SapphireClient } from '@sapphire/framework';
-import { ScheduledTaskRedisStrategy } from '@sapphire/plugin-scheduled-tasks/register-redis';
 import './lib/utils/external/Sanitizer/initClean';
 import '@kaname-png/plugin-statcord/register';
 import Redis from 'ioredis';
@@ -22,7 +21,6 @@ const client = new SapphireClient({
 	},
 	shards: 'auto',
 	intents: ['GUILD_MEMBERS'],
-	tasks: { strategy: new ScheduledTaskRedisStrategy({ bull: { connection: redis } }) },
 	statcord: { 
 		key: process.env.STATCORD_KEY || '', // TODO: this is kind of a hack... look into a better way of doing this later
 		autopost: true,
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,14 +1,3 @@
-export const enum StickNodesCategories {
-    BACKGROUNDS = 'backgrounds',
-    EFFECTS = 'effects',
-    MISCELLANEOUS = 'miscellaneous',
-    OBJECTS = 'objects',
-    PACKS = 'packs',
-    PEOPLE = 'people',
-    WEAPONS = 'weapons',
-    VEHICLES = 'vehicles'
-}
-
 export const enum ModalCustomIds {
     EVAL = 'eval-modal'
   }
